4d5b2a6ed314ff4c3b50b654503df24dae266202
CatinoRosalba <70318528+CatinoRosalba@users.noreply.github.com>
2022-08-16 12:39:56 +0200

    init option menu

--- Assets/Script/Other/MenuController.cs
+++ Assets/Script/Other/MenuController.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Nuovo gioco")]
     public string newGame;
+
+    [Header("Impostazioni - Audio")]
+    [SerializeField] private TMP_Text volumeTextValure = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+    
+    [SerializeField] private GameObject confermationBox = null;
 
     public void newGameDialogYes()
     {
-        SceneManager.LoadScene(newGame);
+        SceneManager.LoadScene(newGame);                                    //Carica una nuova scena
     }
 
     public void newGameDialogNo()
@@ -23,4 +32,35 @@ public class MenuController : MonoBehaviour
         Application.Quit();
         Debug.Log("Uscito");
     }
+
+    public float SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValure.SetText(volume.ToString("0.0").Replace(",", "."));
+        return volume;
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);                 //Salva il valore
+        //StartCoroutine(ConfermationBox());
+    }
+
+    public void ResetButton()
+    {
+        //Audio Reset
+        AudioListener.volume = defaultVolume;
+        volumeSlider.value = defaultVolume;
+        volumeTextValure.SetText(defaultVolume.ToString("0.0").Replace(",", "."));
+        VolumeApply();
+    }
+
+    
+
+    /*public IEnumerator ConfermationBox()
+    {
+        confermationBox.SetActive(true);
+        yield return new WaitForSeconds(2);
+        confermationBox.SetActive(false);
+    }*/
 }
